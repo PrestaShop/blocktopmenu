6816b775ad338a693f153e34d0d736203c60cb5a
jeromenadaud <jerome.nadaud@prestashop.com>
2015-07-01 15:59:07 +0200

    // Version updated to v2.2.0

--- blocktopmenu.php
+++ blocktopmenu.php
@@ -52,7 +52,7 @@ class Blocktopmenu extends Module
 	{
 		$this->name = 'blocktopmenu';
 		$this->tab = 'front_office_features';
-		$this->version = '2.1.1';
+		$this->version = '2.2.0';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
