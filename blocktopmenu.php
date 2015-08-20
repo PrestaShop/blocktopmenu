d2c7e5e8e6e9d832b245610da4113d7b14db77b9
Thomas N <thomas.nabord@prestashop.com>
2015-08-20 14:53:45 +0200

    // Version updated to v2.2.3

--- blocktopmenu.php
+++ blocktopmenu.php
@@ -52,7 +52,7 @@ class Blocktopmenu extends Module
     {
         $this->name = 'blocktopmenu';
         $this->tab = 'front_office_features';
-        $this->version = '2.2.2';
+        $this->version = '2.2.3';
         $this->author = 'PrestaShop';
 
         $this->bootstrap = true;
